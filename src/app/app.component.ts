fac72e39e058e67f48c25ea7f59f2760b5669722
JoelinJeffy <joejeffj3@gmail.com>
2024-07-05 19:43:33 +0530

    in co

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angularmainproject';
-  
+  ngOnInit(): void {
+    const uuid = this.generateUUID();
+    sessionStorage.setItem('uuid', uuid);
+  }
+  private generateUUID(): string {
+    let uuid = '';
+    const chars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < 36; i++) {
+      if (i === 8 || i === 13 || i === 18 || i === 23) {
+        uuid += '-';
+      } else if (i === 14) {
+        uuid += '4';
+      } else if (i === 19) {
+        uuid += chars.charAt(Math.floor(Math.random() * chars.length));
+      } else {
+        uuid += chars.charAt(Math.floor(Math.random() * chars.length));
+      }
+    }
+
+    return uuid;
+  }
 }
